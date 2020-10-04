8c847a8c404bbec8322eb5d6e51d1ab24ca0e11d
Darsh <darsh2987@gmail.com>
2020-10-04 22:06:10 +0100

    added conditional statment for vote_average element and styles

--- src/components/MovieItem.js
+++ src/components/MovieItem.js
@@ -2,13 +2,23 @@ import React from "react";
 
 const IMGPATH = "https://image.tmdb.org/t/p/w1280";
 
+const setVoteClass = (vote) => {
+  if (vote >= 8) {
+    return "movie-item-card-vote-green";
+  } else if (vote >= 5) {
+    return "movie-item-card-vote-orange";
+  } else {
+    return "movie-item-card-vote-red";
+  }
+};
+
 const MovieItem = (props) => {
   return (
     <div className="movie-item-card">
       <img className="movie-item-card-poster" src={IMGPATH + props.movieItem.poster_path} alt={props.movieItem.title} />
       <div className="movie-item-card-info">
         <h3 className="movie-item-card-title">{props.movieItem.title}</h3>
-        <span className="movie-item-card-vote">{props.movieItem.vote_average}</span>
+        <span className={`movie-item-card-vote ${setVoteClass(props.movieItem.vote_average)}`}>{props.movieItem.vote_average}</span>
       </div>
 
       <div className="movie-item-card-overview">
