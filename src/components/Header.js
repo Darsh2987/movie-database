f515160e15c8a92736c26bf969a44b84990979ba
Darsh <darsh2987@gmail.com>
2020-10-04 21:24:31 +0100

    added h1 header and layout styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,6 +3,7 @@ import React from "react";
 const Header = (props) => {
   return (
     <header className="header">
+      <h1>Movie Database</h1>
       <form onSubmit={props.handleInputSubmit}>
         <input className="header-search" type="search" placeholder="Search..." onChange={props.handleInputChange} />
       </form>
